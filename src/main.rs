ba483e648ebf5067db85a6c1250d82d3bda94250
Samuel Bryner <samuelbryner@gmx.ch>
2019-10-05 19:10:32 +0200

    Prints nicely.

--- src/main.rs
+++ src/main.rs
@@ -41,8 +41,23 @@ fn main() {
 
     let mut printer = ThermalPrinter::new(File(printer_handle));
 
+    printer.configure(80, 90, 160).unwrap(); // looks nice
+
     //printer.write("Hello World!"); // why doesn't this exist?!
-    printer.run_test().expect("selftest failed!");
-    printer.feed_n(3).expect("feed failed!");
+    //printer.run_test().expect("selftest failed!");
+    //printer.feed_n(3).expect("feed failed!");
+
+/*
+    for dot in vec![1,10,20,40,80,120,160,200,255] {
+    //for dot in vec![1,10,20,70,75,80,85,90,95,100,105,110,115,120,125,130,70,120,20,120] {
+        printer.configure(80, 80, dot).unwrap();
+        //std::thread::sleep(std::time::Duration::from_millis(200));
+        printer.write_all(format!("### 80/80/interval: {}\n", dot).as_bytes()).unwrap();
+        //write!(&mut printer, "dot time: {}\n", dot).unwrap();
+    }
+    */
+    printer.run_test().unwrap();
+
+    printer.feed_n(10).unwrap();
     printer.flush().unwrap();
 }

6cab2efa89a31b157d0c8d4eee0cfb51cfe15528
Samuel Bryner <samuelbryner@gmx.ch>
2019-10-19 17:37:25 +0200

    Set CWD to where the code is, so we can run it from different places.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import logging
 
 from houdinilib.app import Application
@@ -28,4 +29,10 @@ class ScalePrintingApp(Application):
         print_weight(float(packet['weight']), bool(packet['show_only']), bool(packet['save_as_image']))
 
 if __name__ == "__main__":
+    # quick hack to access files using relative pathnames
+    # I think the real proper way to do this would be to use
+    # importlib.resources, but that seems unnecessarily complicated for our
+    # case (we won't be deploying this application as a zipped egg file)
+    os.chdir(os.path.dirname(__file__))
+
     ScalePrintingApp().run()
